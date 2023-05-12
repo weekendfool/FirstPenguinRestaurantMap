ec960fef64022cabbd733c974d0ba620e1c2852d
尾原徳泰 <nori.tukareta@gmail.com>
2023-05-13 04:18:56 +0900

    podインストール

---
+++ FirstPenguinRestaurantMap/FirstPenguinRestaurantMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstPenguinRestaurantMap
+//
+//  Created by Oh!ara on 2023/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FirstPenguinRestaurantMap/Pods/Target Support Files/Pods-FirstPenguinRestaurantMap/Pods-FirstPenguinRestaurantMap-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FirstPenguinRestaurantMap : NSObject
+@end
+@implementation PodsDummy_Pods_FirstPenguinRestaurantMap
+@end
