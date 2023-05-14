b3808c1d097344ba1a4368bf706c2a3928a320ce
尾原徳泰 <nori.tukareta@gmail.com>
2023-05-15 01:34:43 +0900

    viewModel仮実装

--- FirstPenguinRestaurantMap/FirstPenguinRestaurantMap/ViewModel/ConditionalInputViewModel.swift
+++ FirstPenguinRestaurantMap/FirstPenguinRestaurantMap/ViewModel/ConditionalInputViewModel.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+
+
+protocol ConditionalInputViewModelType {
+    associatedtype viewModelInput
+    associatedtype viewModelOutput
+    
+    func transform(input: viewModelInput) -> viewModelOutput
+}
+
+
+final class ConditionalInputViewModel {
+
+}
+
+extension ConditionalInputViewModel: ConditionalInputViewModelType {
+   
+    
+    
+    struct viewModelInput {
+        // 画面の展開
+        let isMadeStoryboard: Observable<[Any]>
+        // スライダー
+        let inputDistanceSlider: Observable<Int>
+        // button
+        let tappedSearchButton: Signal<Void>
+        // mapView
+        let inputMapView: Signal<Void>
+        
+        // 電波の状況
+        let comunnicationState: Observable<[Any]>
+    }
+    
+    struct viewModelOutput {
+        // 距離
+        let distance: Driver<Float>
+    }
+    
+    
+    func transform(input: viewModelInput) -> viewModelOutput {
+        <#code#>
+    }
+    
+}
