2ce6a5122f2a995bbd28076e60201e43f546f651
尾原徳泰 <nori.tukareta@gmail.com>
2023-05-15 01:34:18 +0900

    APIModel仮実装

--- FirstPenguinRestaurantMap/FirstPenguinRestaurantMap/Model/APIModel.swift
+++ FirstPenguinRestaurantMap/FirstPenguinRestaurantMap/Model/APIModel.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+import Alamofire
+
+final class APIModel {
+    
+    // MARK: - 変数
+    
+    // URL
+    
+    // apiKey
+    let apiKey = "cfbf72a9451eb428"
+    var keyWord = ""
+    
+    // 緯度
+    var lat = ""
+    // 経度
+    var lng = ""
+    // 範囲: 1-5まで
+    var range: range = .first
+    
+    
+    
+    // MARK: - 関数
+    
+    func getData(lat: String, lng: String, range: range) {
+        let urlString = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=\(apiKey)&lat=\(lat)&lng=\(lng)&range=\(range.isValue)"
+    }
+    
+    // 情報の取得
+}
+
+// 検索範囲の段階
+enum range: Codable {
+    case first
+    case second
+    case third
+    case fourth
+    case fifth
+}
+
+extension range {
+    var isValue: String {
+        switch self {
+        case .first:
+            return "1"
+        case .second:
+            return "2"
+        case .third:
+            return "3"
+        case .fourth:
+            return "4"
+        case .fifth:
+            return "5"
+        }
+    }
+}
