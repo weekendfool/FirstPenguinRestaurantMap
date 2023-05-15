db7146e3dc66ceb37dd1d3b279e3155632413c71
尾原徳泰 <nori.tukareta@gmail.com>
2023-05-15 17:39:09 +0900

    ConditionalInputViewController修正 viewModel紐付け

--- FirstPenguinRestaurantMap/FirstPenguinRestaurantMap/View/ConditionalInputView/ConditionalInputViewController.swift
+++ FirstPenguinRestaurantMap/FirstPenguinRestaurantMap/View/ConditionalInputView/ConditionalInputViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import MapKit
+import RxSwift
+import RxCocoa
 
 class ConditionalInputViewController: UIViewController {
     
@@ -18,6 +20,14 @@ class ConditionalInputViewController: UIViewController {
     @IBOutlet weak var distanceLabel: UILabel!
     
     // MARK: - 変数
+    let viewModel = ConditionalInputViewModel()
+    let disposeBag: DisposeBag = DisposeBag()
+    // 通信エラー検知用
+    private let nWPathMonitorModel: NWPathMonitorModel = NWPathMonitorModel()
+    
+    var userLocation = MKUserLocation()
+    
+    
     // MARK: - ライフサイクル
    
    
@@ -31,7 +41,7 @@ class ConditionalInputViewController: UIViewController {
         var locationManager = CLLocationManager()
         CLLocationManager.locationServicesEnabled()
         
-        var userLocation = MKUserLocation()
+        
         let status = CLLocationManager.authorizationStatus()
         
         if status == CLAuthorizationStatus.notDetermined {
@@ -42,6 +52,7 @@ class ConditionalInputViewController: UIViewController {
         mapView.userTrackingMode = .follow
         print("userLocation: \(userLocation)")
         
+        let x = userLocation.coordinate.latitude
         // 祝作
         var ragion = mapView.region
         
@@ -51,15 +62,80 @@ class ConditionalInputViewController: UIViewController {
         
         mapView.setRegion(ragion, animated: true)
 
-        // Do any additional setup after loading the view.
+        // viewmodelとの紐付け
+        bindViewModel()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        getNetworkState(state: networkstate.successfulCommunication as Any)
+
     }
     
 
     // MARK: - 関数
+    
+    // 通信のセットアップ
+    func setupMonitorComunnication() {
+        nWPathMonitorModel.monitorComunnication { state  in
+             self.getNetworkState(state: state as Any)
+         }
+         
+         nWPathMonitorModel.start()
+    }
+    
+    // 紐付け
+    func bindViewModel() {
+        let isMadeStoryboard = rx.methodInvoked(#selector(viewWillAppear(_:)))
+        let comunnicationState = rx.methodInvoked(#selector(getNetworkState))
+        let myPosition = rx.methodInvoked(#selector(getter: mapView))
+        
+        let input = ConditionalInputViewModel.viewModelInput(
+            isMadeStoryboard: isMadeStoryboard,
+            myPosition: myPosition,
+            tappedSearchButton: searchButton.rx.tap.asSignal(),
+            comunnicationState: comunnicationState
+        )
+        
+        let output = viewModel.transform(input: input)
+        
+        // 通信状況
+        output.comunnicationState
+            .drive { [weak self] state in
+                
+            }
+            .disposed(by: disposeBag)
+        
+        output.getData
+            .drive { data in
+                print("----------------")
+                print("data: \(data)")
+            }
+            .disposed(by: disposeBag)
+        
+        // 画面遷移
+        output.goIntuitionSelectView
+            .drive { info in
+                print("----------------")
+                print("info: \(info)")
+            }
+            .disposed(by: disposeBag)
+    }
+    
 }
 
 // MARK: - extension
 
 extension ConditionalInputViewController: MKMapViewDelegate {
-   
+    //　自己位置更新
+    @objc func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        print("更新")
+    }
+}
+
+extension ConditionalInputViewController {
+    @objc func getNetworkState(state: Any) {
+        // 登録する
+    }
 }
